a68e465cfa87d6e7f9d44b9a73eb484381c994ec
AiEson <32814260+AiEson@users.noreply.github.com>
2022-02-10 00:47:18 +0800

    update

--- LeetCode/src/最长递增子序列.java
+++ LeetCode/src/最长递增子序列.java
@@ -8,15 +8,14 @@ public class 最长递增子序列 {
         public int lengthOfLIS(int[] nums) {
             //dp[i] 为第 i 个时的最长子序列的长度
             int[] dp = new int[2510];
-            dp[0] = 1;
-            int max = 1;
-            for (int i = 1; i < nums.length; i++) {
+            Arrays.fill(dp, 1);
+            int max = -1;
+            for (int i = 0; i < nums.length; i++) {
                 for (int j = 0; j <= i; j++) {
                     if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
                 max = Math.max(max, dp[i]);
             }
-            System.out.println(Arrays.toString(dp));
             return max;
         }
     }
